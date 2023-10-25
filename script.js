5c1593d5b05a75418a58b4e573c73d3ae2610b8f
Anvar <anvar@bilgakhan.uz>
2023-10-25 11:55:40 +0500

    nimkjwrgiwrksrj

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document
     const API_KEY = "6496848472:AAHX7F5Zz5DWX59GW4LuTz2MTygr-zvj7TE";
     const chatId = "5692222234";
 
-    const message = `Siz yangi xabar keldi!\nIsmi: ${username}\nTelefon raqami: ${phone}\nManzil: ${address}\nOrientri: ${orientr}`;
+    const message = `You have a new message!\nName: ${username}\nPhone: ${phone}\nAddress: ${address}\nOrientr: ${orientr}`;
 
     const url = `https://api.telegram.org/bot${API_KEY}/sendMessage`;
     const params = new URLSearchParams({
@@ -30,18 +30,18 @@ document
       .then((response) => {
         if (response.ok) {
           alert(
-            "Xabaringiz yuborildi. Orqada qaytishingiz mumkun. Siz bilan yaqin orada bog'lanamiz"
+            "Your message has been sent. We will get back to you shortly. Thank you!"
           );
           document.getElementById("username").value = "";
           document.getElementById("phone").value = "";
           document.getElementById("address").value = "";
           document.getElementById("orientr").value = "";
         } else {
-          alert("Oppps. Xabaringiz yuborilmadi. Qaytadan urunib ko'ring");
+          alert("Oops! Failed to send your message. Please try again.");
         }
       })
       .catch((error) => {
-        console.error("Xabarni yuborishda xatolik:", error);
-        alert("Xatolik yuzaga keladi. Qayta urunib ko'ring");
+        console.error("Error sending message:", error);
+        alert("An error occurred. Please try again.");
       });
   });
